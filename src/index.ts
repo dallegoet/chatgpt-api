53941696f1c6e8ae9744caafadf64b968778b29e
Dimitri Allegoet <dallegoet@gmail.com>
2022-12-04 02:07:24 +0100

    cleaning

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,50 @@
-import { uuid } from "uuidv4";
+import { v4 } from "uuid";
+import fetch from "node-fetch";
 
 export default class ChatGPTClient {
+  readonly defaultModel = "text-davinci-002-render";
+  readonly baseUri = "https://chat.openai.com";
+  readonly baseHeaders = {
+    accept: "*/*",
+    "accept-language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
+    "if-none-match": '"11xxpujbgv2148"',
+    "content-type": "application/json",
+    "sec-ch-ua": '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    Referer: "https://chat.openai.com/chat",
+    "Referrer-Policy": "strict-origin-when-cross-origin",
+  };
+
   accessToken: string | null = null;
   conversationId: string | null = null;
-  lastMessageId: string = uuid();
+  lastMessageId: string = v4();
 
   public async loginFromNextAuthSessionToken(nextAuthSessionToken: string) {
-    const response = await fetch("https://chat.openai.com/api/auth/session", {
-      headers: {
-        accept: "*/*",
-        "accept-language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
-        "if-none-match": '"11xxpujbgv2148"',
-        "sec-ch-ua": '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": '"Windows"',
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin",
-        cookie: `__Secure-next-auth.session-token=${nextAuthSessionToken}`,
-        Referer: "https://chat.openai.com/chat",
-        "Referrer-Policy": "strict-origin-when-cross-origin",
-      },
-      body: null,
+    const response = await fetch(`${this.baseUri}/api/auth/session`, {
       method: "GET",
+      headers: {
+        ...this.baseHeaders,
+        cookie: `__Secure-next-auth.session-token=${nextAuthSessionToken}`,
+      },
     });
 
-    const { accessToken, ...rest } = await response.json();
+    const { accessToken } = (await response.json()) as { accessToken: string };
     this.accessToken = accessToken;
-  }
-
-  private async throwIfNotLogged() {
-    if (this.accessToken === null) {
-      throw new Error('client is not logged, you must call ChatGPTClient.loginFromNextAuthSessionToken("YOUR_TOKEN") before.');
-    }
   }
 
   public async getModels() {
     this.throwIfNotLogged();
 
-    const response = await fetch("https://chat.openai.com/backend-api/models", {
-      headers: {
-        accept: "*/*",
-        "accept-language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
-        authorization: `Bearer ${this.accessToken}`,
-        "content-type": "application/json",
-        "sec-ch-ua": '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": '"Windows"',
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin",
-        Referer: "https://chat.openai.com/chat",
-        "Referrer-Policy": "strict-origin-when-cross-origin",
-      },
-      body: null,
+    const response = await fetch(`${this.baseUri}/backend-api/models`, {
       method: "GET",
+      headers: {
+        ...this.baseHeaders,
+        authorization: `Bearer ${this.accessToken}`,
+      },
     });
 
     return await response.json();
@@ -62,17 +52,17 @@ export default class ChatGPTClient {
 
   public async startNewConversation() {
     this.conversationId = null;
-    this.lastMessageId = uuid();
+    this.lastMessageId = v4();
   }
 
-  public async write(text: string) {
+  public async write(text: string, model = this.defaultModel) {
     this.throwIfNotLogged();
 
     const body = JSON.stringify({
       action: "next",
       messages: [
         {
-          id: uuid(),
+          id: v4(),
           role: "user",
           content: {
             content_type: "text",
@@ -81,30 +71,17 @@ export default class ChatGPTClient {
         },
       ],
       parent_message_id: this.lastMessageId,
-      model: "text-davinci-002-render",
+      model,
       ...(this.conversationId ? { conversation_id: this.conversationId } : {}),
     });
 
-    const response = await fetch("https://chat.openai.com/backend-api/conversation", {
-      headers: {
-        accept: "text/event-stream",
-        "accept-language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
-        authorization: `Bearer ${this.accessToken}`,
-        "content-type": "application/json",
-        "sec-ch-ua": '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": '"Windows"',
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin",
-        "x-openai-assistant-app-id": "",
-      },
-      referrer: "https://chat.openai.com/chat",
-      referrerPolicy: "strict-origin-when-cross-origin",
+    const response = await fetch(`${this.baseUri}/backend-api/conversation`, {
       body,
       method: "POST",
-      mode: "cors",
-      credentials: "include",
+      headers: {
+        ...this.baseHeaders,
+        authorization: `Bearer ${this.accessToken}`,
+      },
     });
 
     const fullResponseStream = await response.text();
@@ -115,5 +92,11 @@ export default class ChatGPTClient {
     this.lastMessageId = lastEvent.message.id;
 
     return lastEvent.message.content.parts;
+  }
+
+  private async throwIfNotLogged() {
+    if (this.accessToken === null) {
+      throw new Error('client is not logged, you must call client.loginFromNextAuthSessionToken("YOUR_TOKEN") before.');
+    }
   }
 }
